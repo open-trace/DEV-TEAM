5b9b46bece144d617c5dcc30fb347c95fe90415c
Aliyaopentrace <aliya.opentrace@gmail.com>
2026-01-27 07:56:53 -0500

    Create aiService.js

---
+++ src/aiService.js
@@ -0,0 +1,10 @@
+// This will be replaced when Tobi's AI API is ready
+
+const askAI = async (question) => {
+  // Placeholder response
+  return `AI response for: "${question}"`;
+};
+
+module.exports = {
+  askAI,
+};
